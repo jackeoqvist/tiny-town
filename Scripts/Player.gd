969d6793f9dfb3e86970eb69364ac104064c284f
Jakob <jacke98@hotmail.se>
2025-08-06 15:11:06 +0200

    Added basic PlayerController and a menu scene

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,4 +1,17 @@
 extends CharacterBody2D
 
+@export var speed : float = 300
+
 func _ready() -> void:
 	print("Hello")
+
+func _physics_process(delta: float) -> void:
+	
+	var direction = Input.get_vector("left", "right", "up", "down")
+	
+	if direction.length() > 0:
+		direction = direction.normalized()
+	
+	velocity = direction * speed * delta
+	
+	move_and_slide()
